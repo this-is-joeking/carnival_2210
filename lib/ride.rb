1256b564b5fad0407f153a70d51be36bf9fcc887
Joe King <j.king92@gmail.com>
2022-11-14 09:39:28 -0700

    feat: #initialize and add attr_reader

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,15 @@
 class Ride
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue
 
+  def initialize(ride_data)
+    @name = ride_data[:name]
+    @min_height = ride_data[:min_height]
+    @admission_fee = ride_data[:admission_fee]
+    @excitement = ride_data[:excitement]
+    @total_revenue = 0
+  end
 end
