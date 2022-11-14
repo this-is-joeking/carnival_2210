e2374e9fb292f91c34fe80b83b81e6186c2e638d
Joe King <j.king92@gmail.com>
2022-11-14 09:20:38 -0700

    feat: add #initialize, #format_money helper and associated test

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,18 @@
 class Visitor
+  attr_reader :name,
+              :height,
+              :spending_money,
+              :preferences
 
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = format_money(spending_money)
+    @preferences = []
+  end
+
+  def format_money(string)
+    return string[1..].to_i if string.is_a?(String) && string[0] == '$'
+    string.to_i
+  end
 end
--- spec/vistor_spec.rb
+++ spec/vistor_spec.rb
@@ -2,8 +2,27 @@ require './lib/visitor'
 
 RSpec.describe Visitor do
   it 'exists' do
-    visitor = Visitor.new
+    visitor = Visitor.new('jonny', 62, '$101')
 
-    expect(vistor).to be_a Visitor
+    expect(visitor).to be_a Visitor
+  end
+
+  it 'has height, name, preferences, and spending money' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    expect(visitor1.name).to eq('Bruce')
+    expect(visitor1.height).to eq(54)
+    expect(visitor1.spending_money).to eq(10)
+    expect(visitor1.preferences).to eq([])
+  end
+
+  describe '#format_money' do
+    it 'takes a dollar value string and turns it to an integer' do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      
+      expect(visitor1.format_money('$123_000')).to eq(123_000)
+      expect(visitor1.format_money(123_000)).to eq(123_000)
+      expect(visitor1.format_money('123_000')).to eq(123_000)
+    end
   end
 end
