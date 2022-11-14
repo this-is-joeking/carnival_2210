319483effe872a29e210292a04600ffc3f9b9750
Joe King <j.king92@gmail.com>
2022-11-14 11:24:29 -0700

    feat: #most_profitable_ride

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -25,4 +25,11 @@ class Carnival
       most_riders.number_of_riders == ride.number_of_riders
     end
   end
+
+  def most_profitable_ride
+    most_money_made = @rides.max_by { |ride| ride.total_revenue }
+    @rides.find_all do |ride|
+      most_money_made.total_revenue == ride.total_revenue
+    end
+  end
 end
