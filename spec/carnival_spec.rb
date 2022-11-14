78a8d05835a0dcb2d46fd6c4b0a8646e2c12b6dd
Joe King <j.king92@gmail.com>
2022-11-14 10:56:59 -0700

    test: #most_popular_ride

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -38,18 +38,63 @@ RSpec.describe Carnival do
       ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
       ride2 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
       ride3 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3)    
       visitor1 = Visitor.new('Bruce', 54, '$10')
       visitor2 = Visitor.new('Tucker', 36, '$50')
+      visitor3 = Visitor.new('April', 59, '$1000')
       visitor1.add_preference(:thrilling)
       visitor2.add_preference(:gentle)
       
       expect(carnival.total_revenue).to eq(0)
 
-      3.times { ride1.board_rider(visitor2) } #3
-      4.times { ride2.board_rider(visitor1) } #8
-      5.times { ride3.board_rider(visitor2) } #25
+      3.times { ride1.board_rider(visitor2) }
+      4.times { ride2.board_rider(visitor1) }
+      5.times { ride3.board_rider(visitor2) }
 
       expect(carnival.total_revenue).to eq(36)
+
+      10.times {ride3.board_rider(visitor3) }
+
+      expect(carnival.total_revenue).to eq(36)
+    end
+  end
+
+  describe '#most_popular_ride' do
+    it 'returns the ride that has had the most riders' do
+      carnival = Carnival.new('4 Corners Carnival', '30 days')
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride2 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+      ride3 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+      carnival.add_ride(ride1)
+      carnival.add_ride(ride2)
+      carnival.add_ride(ride3) 
+      visitor1 = Visitor.new('Bruce', 60, '$1000')
+      visitor2 = Visitor.new('Tucker', 54, '$1000')
+      visitor3 = Visitor.new('April', 59, '$1000')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      visitor3.add_preference(:gentle)
+      visitor1.add_preference(:thrilling)
+      visitor2.add_preference(:thrilling)
+      visitor3.add_preference(:thrilling)
+
+      5.times { ride1.board_rider(visitor1) }
+      3.times { ride1.board_rider(visitor2) }
+      2.times { ride1.board_rider(visitor3) }
+      1.times { ride2.board_rider(visitor1) }
+      3.times { ride2.board_rider(visitor2) }
+      1.times { ride2.board_rider(visitor3) }
+      2.times { ride3.board_rider(visitor1) }
+      5.times { ride3.board_rider(visitor2) }
+      1.times { ride3.board_rider(visitor3) }
+
+      expect(carnival.most_popular_ride).to eq([ride1])
+
+      2.times { ride3.board_rider(visitor3) }
+
+      expect(carnival.most_popular_ride).to eq([ride1, ride3])
     end
   end
 end
